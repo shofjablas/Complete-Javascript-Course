e2660a607dbd9f6cf53f2dc46218fac951be3098
shofjablas <shluxnsal01@gmail.com>
2023-01-20 01:07:29 +0900

    null까지 공부

--- 01-Fundamentals-1/script.js
+++ 01-Fundamentals-1/script.js
@@ -1,2 +1,42 @@
-let js = "amazing";
+/* let js = "amazing";
 console.log(40 + 8 + 23 - 10);
+
+console.log("Jonas");
+console.log(23);
+
+let firstName = "Lisa";
+
+console.log(firstName);
+
+let jonas_lisa = "jl";
+let $function = 27;
+
+let person = "jonas";
+let PI = 3.14;
+
+let myFirstJob = "Programmer";
+let myCurrentJob = "Teacher";
+
+let job1 = "Programmer";
+let job2 = "Teacher"; */
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof "Jonas");
+
+javascriptIsFun = "yes!";
+console.log(javascriptIsFun);
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 2023;
+console.log(year);
+
+console.log(typeof null);
