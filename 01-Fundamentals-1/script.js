1869816efdfcd3eb467e29fd36899469267bc4cd
shofjablas <shluxnsal01@gmail.com>
2023-01-30 03:03:35 +0900

    type conversion 어렵네 ㅋㅋ

--- 01-Fundamentals-1/script.js
+++ 01-Fundamentals-1/script.js
@@ -138,3 +138,22 @@ console.log(typeof null); */
 // }
 
 // console.log(century);
+
+//type conversion
+const inputYear = "1991";
+console.log(Number(inputYear), inputYear);
+console.log(inputYear + 18);
+
+console.log(Number("Jonas"));
+console.log(typeof NaN);
+
+console.log(String(23), 23);
+
+//type coercion
+console.log("I am " + 23 + "years old");
+console.log("I am " + "23" + "years old");
+
+console.log("23" - "10" - 3);
+console.log("23" + "10" + 3);
+console.log("23" * "2");
+console.log("23" / "2");
