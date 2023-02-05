160c950b3d4d94b7da04c29d63442838601b1e05
shofjablas <shluxnsal01@gmail.com>
2023-02-05 19:06:19 +0900

    "use strict"

--- 02-Fundamentals-2/script.js
+++ 02-Fundamentals-2/script.js
@@ -1,1 +1,10 @@
 "use strict";
+
+let hasDriverslicense = false;
+const passTest = true;
+
+if (passTest) hasDriverlicense = true;
+if (hasDriverslicense) console.log("I can drive");
+
+//const interface = "Audio";
+const private = 543;
