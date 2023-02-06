76c94f5252787127edee32c601fea56043ae8f9e
shofjablas <shluxnsal01@gmail.com>
2023-02-07 02:06:56 +0900

    function..

--- 02-Fundamentals-2/script.js
+++ 02-Fundamentals-2/script.js
@@ -16,3 +16,18 @@ function logger() {
 
 //calling / running / invoking functions
 logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+  console.log(apples, oranges);
+  const juice = `juice with ${apples} apples and ${oranges} oranges.`;
+  return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+//console.log(fruitProcessor(5, 0));
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
