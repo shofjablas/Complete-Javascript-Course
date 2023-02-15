5763c0866f4ba670ef4d35b681708a549de9abd1
shofjablas <shluxnsal01@gmail.com>
2023-02-15 19:58:43 +0900

    함수안에 rif문 추가

--- 02-Fundamentals-2/script.js
+++ 02-Fundamentals-2/script.js
@@ -61,12 +61,48 @@ console.log(appleOrangeJuice);
 // const age3 = calcAge3(1991);
 // console.log(age3);
 
-const yearsUltRetirement = (bithYear, firstname) => {
-  const age = 2037 - bithYear;
-  const retirement = 65 - age;
-  // return retirement;
-  return `${firstname} retires in ${retirement} years`;
+// const yearsUltRetirement = (bithYear, firstname) => {
+//   const age = 2037 - bithYear;
+//   const retirement = 65 - age;
+// return retirement;
+//   return `${firstname} retires in ${retirement} years`;
+// };
+
+// console.log(yearsUltRetirement(1991, "Jonas"));
+// console.log(yearsUltRetirement(1980, "Bob"));
+
+// function cutFruitPieces(fruit) {
+//   return fruit * 4;
+// }
+
+// function fruitProcessor(apples, oranges) {
+//   const applePieces = cutFruitPieces(apples);
+//   const orangesPieces = cutFruitPieces(oranges);
+
+//   console.log(apples, oranges);
+//   const juice = `juice with ${applePieces} picee of apple and ${orangesPieces} picee of orange.`;
+//   return juice;
+// }
+
+// console.log(fruitProcessor(2, 3));
+
+const calcAge = function (birthYear) {
+  return 2037 - birthYear;
 };
 
-console.log(yearsUltRetirement(1991, "Jonas"));
-console.log(yearsUltRetirement(1980, "Bob"));
+const yearsUltRetirement = function (birthYear, firstname) {
+  const age = calcAge(birthYear);
+  const retirement = 65 - age;
+
+  if (retirement > 0) {
+    //0이상이면, 0이하이면 다른 걸 반환한다. 따라서 단순히 은퇴 금액을 반환하는 대신 은퇴 금액에 따른 결정을 해 본다.
+    console.log(`${firstname} retires in ${retirement} years`);
+    return retirement;
+  } else {
+    console.log(`${firstname} has already retired!`);
+    return -1;
+  }
+};
+//if블록에 어떤 코드든 넣을 수 있다.
+console.log(yearsUltRetirement(1991, "Jonas")); //1991은 93번째 줄의 birthYear이다.
+console.log(yearsUltRetirement(1950, "Mike"));
